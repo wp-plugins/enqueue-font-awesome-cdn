11077b33bb1f6dfed4d8651104ed6f5bf40af6ab
jp2112 <jp2112@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-16 22:19:20 +0000

    first upload of new plugin
    git-svn-id: https://plugins.svn.wordpress.org/enqueue-font-awesome-cdn/trunk@949943 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ enqueue-font-awesome-cdn.php
@@ -0,0 +1,62 @@
+<?php
+/*
+Plugin Name: Enqueue Font Awesome CDN
+Plugin URI: http://www.jimmyscode.com/wordpress/enqueue-font-awesome-cdn/
+Description: Automatically enqueue <a href="http://fortawesome.github.io/Font-Awesome/">Font Awesome</a> from CDN
+Version: 0.0.1
+Author: Jimmy Pe&ntilde;a
+Author URI: http://www.jimmyscode.com/
+License: GPLv2 or later
+*/
+
+if (!defined('EFAC_PLUGIN_NAME')) {
+	// plugin constants
+	define('EFAC_PLUGIN_NAME', 'Enqueue Font Awesome');
+	define('EFAC_SLUG', 'enqueue-font-awesome-cdn');
+	define('EFAC_LOCAL', 'efac');
+	define('EFAC_FA_VERSION', '4.1.0');
+}
+// oh no you don't
+if (!defined('ABSPATH')) {
+	wp_die(__('Do not access this file directly.', efac_get_local()));
+}
+
+// localization to allow for translations
+add_action('init', 'efac_translation_file');
+function efac_translation_file() {
+	$plugin_path = plugin_basename(dirname(__FILE__) . '/translations');
+	load_plugin_textdomain(efac_get_local(), '', $plugin_path);
+}
+
+// main function
+// based on http://fortawesome.github.io/Font-Awesome/get-started/ and 
+// http://wpbacon.com/tutorials/font-awesome-wordpress-cdn/
+add_action('wp_enqueue_scripts', 'efac_load_fa_from_cdn');
+function efac_load_fa_from_cdn() {
+	wp_enqueue_style(
+		efac_get_slug(),
+		'//maxcdn.bootstrapcdn.com/font-awesome/' . EFAC_FA_VERSION . '/css/font-awesome.min.css',
+		array(),
+		EFAC_FA_VERSION,
+		'all'
+	);
+}
+	// add helpful links to plugin page next to plugin name
+	// http://wpengineer.com/1295/meta-links-for-wordpress-plugins/
+	add_filter('plugin_row_meta', 'efac_meta_links', 10, 2);
+	function efac_meta_links($links, $file) {
+		if ($file == plugin_basename(__FILE__)) {
+			$links = array_merge($links,
+			array(
+				sprintf(__('<a href="http://wordpress.org/support/plugin/%s">Support</a>', efac_get_local()), efac_get_slug()),
+				sprintf(__('<a href="http://wordpress.org/extend/plugins/%s/">Documentation</a>', efac_get_local()), efac_get_slug()),
+				sprintf(__('<a href="http://wordpress.org/plugins/%s/faq/">FAQ</a>', efac_get_local()), efac_get_slug())
+			));
+		}
+		return $links;	
+	}
+
+// encapsulate these and call them throughout the plugin instead of hardcoding the constants everywhere
+	function efac_get_slug() { return EFAC_SLUG; }
+	function efac_get_local() { return EFAC_LOCAL; }
+?>
---
+++ readme.txt
@@ -0,0 +1,53 @@
+=== Enqueue Font Awesome CDN ===
+Tags: font awesome, cdn, enqueue, script
+Requires at least: 3.5
+Tested up to: 3.9
+Contributors: jp2112
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=7EX9NB9TLFHVW
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Automatically enqueue the Font Awesome font and CSS toolkit from the MaxCDN network.
+
+== Description ==
+
+This plugin automatically enqueues the Font Awesome CSS file from the MaxCDN CDN. Don't put the code in your theme, use this plugin instead. Then use the Font Awesome CSS tags as normal.
+
+Based on http://wpbacon.com/tutorials/font-awesome-wordpress-cdn/ and http://fortawesome.github.io/Font-Awesome/get-started/
+
+Disclaimer: This plugin is not affiliated with or endorsed by the creators of Font Awesome.
+
+== Installation ==
+
+1. Upload plugin file through the WordPress interface.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+3. Load any page or post and check the source for a link to the Font Awesome CSS file.
+
+== Frequently Asked Questions ==
+
+= How do I use the plugin? =
+
+Simply install and activate the plugin. It starts working immediately with no configuration required. All pages will load the Font Awesome CSS file which you can use to style anything you want.
+
+= I activated the plugin but I don't see the link in my page source. =
+
+Are you caching the page?
+
+= How do I do X with Font Awesome? =
+
+I have no idea. Check their <a href="http://fortawesome.github.io/Font-Awesome/icons/">icons</a> and <a href="http://fortawesome.github.io/Font-Awesome/examples/">examples</a> pages for example usage.
+
+== Screenshots ==
+
+1. Plugin output
+
+== Changelog ==
+
+= 0.0.1 =
+- created
+
+== Upgrade Notice ==
+
+= 0.0.1 =
+- created
